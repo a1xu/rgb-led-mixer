419c6c0fed15e335229b4fc7477c03528e585182
a1xu <a1xu@ucsd.edu>
2017-10-28 23:54:46 -0700

    Added Arduino code, Fritzing

---
+++ PiB_RGB_LED_Mixer/PiB_RGB_LED_Mixer.ino
@@ -0,0 +1,60 @@
+#define COMMON_ANODE
+
+// Pin assignments //
+const int r = 10;
+const int g = 9;
+const int b = 8;
+const int pot_r = A2;
+const int pot_g = A1;
+const int pot_b = A0;
+
+// Initialize values //
+int r_val = 0;
+int g_val = 0;
+int b_val = 0;
+int pot_r_val = 0;
+int pot_g_val = 0;
+int pot_b_val = 0;
+int out_r = 0;
+int out_g = 0;
+int out_b = 0;
+
+void setup() {
+  // Pin mode assignment //
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Get potentiometer values //
+  pot_r_val = analogRead(r);
+  pot_g_val = analogRead(g);
+  pot_b_val = analogRead(b);
+//  Serial.print(pot_r_val);
+//  Serial.print(pot_g_val);
+//  Serial.print(pot_b_val);
+  
+  // Map potentiometer values to LED color channels //
+  out_r = map(pot_r_val, 0, 1023, 0, 255);
+  out_g = map(pot_g_val, 0, 1023, 0, 255);
+  out_b = map(pot_b_val, 0, 1023, 0, 255);
+
+  #ifdef COMMON_ANODE
+  out_r = 255 - out_r;
+  out_g = 255 - out_g;
+  out_b = 255 - out_b;
+  #endif
+//  Serial.print(out_r);
+//  Serial.print(out_g);
+//  Serial.print(out_b);
+  
+  // Update LED color channels //
+  analogWrite(r, out_r);
+  analogWrite(g, out_g);
+  analogWrite(b, out_b);
+
+  // Wait 10 ms //
+  delay(10);
+}
